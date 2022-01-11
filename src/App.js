12722c65138a1a8392b1d89f3ad677f48239eefa
ku0513 <kenichi.umeda@gmail.com>
2022-01-11 23:44:35 +0900

    レクチャー26 (TaskCardsコンポーネントを作成しよう)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header/Header";
+import { TaskCards } from "./components/Task/TaskCards";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <TaskCards />
     </div>
   );
 }
