4228d0375dc5d7564bb8dcb7f6b922aa80741bbf
ku0513 <kenichi.umeda@gmail.com>
2022-01-12 23:53:06 +0900

    レクチャー27 (TaskCardコンポーネントを作成しよう

--- src/components/Task/TaskCard.jsx
+++ src/components/Task/TaskCard.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { TaskCardDeleteButton } from "./Button/TaskCardDeleteButton";
+import { TaskAddInput } from "./Input/TaskAddInput";
+import { TaskCardTitle } from "./TaskCardTitle";
+import { Tasks } from "./Tasks";
 
 export const TaskCard = () => {
-  return <div></div>;
+  return (
+    <div className="taskCard">
+      <TaskCardTitle />
+      <TaskCardDeleteButton />
+      <TaskAddInput />
+      <Tasks />
+    </div>
+  );
 };
