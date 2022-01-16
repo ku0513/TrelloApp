5cf3bdd2b22d8a76f916a3c94e0b370b29cd5969
ku0513 <kenichi.umeda@gmail.com>
2022-01-16 19:39:27 +0900

    レクチャー30 (TaskAddInputコンポーネントを作成しよう)

--- src/components/Task/Input/TaskAddInput.jsx
+++ src/components/Task/Input/TaskAddInput.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export const TaskAddInput = () => {
-  return <div></div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="add a task" className="taskAddInput" />
+      </form>
+    </div>
+  );
 };
