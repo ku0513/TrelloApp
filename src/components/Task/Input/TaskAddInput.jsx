10daf06a5490a5625b96bcc44548a83381da6ce6
ku0513 <kenichi.umeda@gmail.com>
2022-01-17 23:13:37 +0900

    レクチャー31 (入力内容をstateで管理してみよう)

--- src/components/Task/Input/TaskAddInput.jsx
+++ src/components/Task/Input/TaskAddInput.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 
-export const TaskAddInput = () => {
+export const TaskAddInput = (
+  inputText,
+  setInputText,
+  taskList,
+  setTaskList
+) => {
   const handleSubmit = (e) => {
     e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="add a task" className="taskAddInput" />
+        <input
+          type="text"
+          placeholder="add a task"
+          className="taskAddInput"
+          onChange={handleChange}
+        />
       </form>
     </div>
   );
--- src/components/Task/TaskCard.jsx
+++ src/components/Task/TaskCard.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { TaskCardDeleteButton } from "./Button/TaskCardDeleteButton";
 import { TaskAddInput } from "./Input/TaskAddInput";
 import { TaskCardTitle } from "./TaskCardTitle";
 import { Tasks } from "./Tasks";
 
 export const TaskCard = () => {
+  const [inputText, setInputText] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
   return (
     <div className="taskCard">
       <TaskCardTitle />
       <TaskCardDeleteButton />
-      <TaskAddInput />
+      <TaskAddInput
+        inputText={inputText}
+        setInputText={setInputText}
+        taskList={taskList}
+        setTaskList={setTaskList}
+      />
       <Tasks />
     </div>
   );
