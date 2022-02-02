cc5a8c032b3bf9ea18546203192e17f42b1ef385
ku0513 <kenichi.umeda@gmail.com>
2022-02-03 00:27:25 +0900

    レクチャー41 (ボタンを押してタスクカードを追加してみよう)

--- src/components/Task/TaskCards.jsx
+++ src/components/Task/TaskCards.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { AddTaskCardButton } from "./Button/AddTaskCardButton";
 import { TaskCard } from "./TaskCard";
 
 export const TaskCards = () => {
+  const [taskCardsList, setTaskCardsList] = useState([
+    { id: 0, dragabbleId: "item0" },
+  ]);
+
   return (
     <div className="taskCardsArea">
-      <TaskCard />
-      <AddTaskCardButton />
+      {taskCardsList.map((taskCardList) => (
+        <TaskCard key={taskCardList.id} />
+      ))}
+      <AddTaskCardButton
+        taskCardsList={taskCardsList}
+        setTaskCardsList={setTaskCardsList}
+      />
     </div>
   );
 };
